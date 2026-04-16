08c515a6aa1075450cea92ebca65032eb9d7ad83
Tarun Gola <tarungola205@gmail.com>
2026-04-16 13:40:31 +0530

    Update Some Files

--- DSA/src/AlgorithmDesignAndTechniques/DivideAndConquer/Lec1_DivideAndConquerTechnique.java
+++ DSA/src/AlgorithmDesignAndTechniques/DivideAndConquer/Lec1_DivideAndConquerTechnique.java
@@ -3,6 +3,17 @@ package AlgorithmDesignAndTechniques.DivideAndConquer;
 public class Lec1_DivideAndConquerTechnique {
     public static void main(String[] args) {
         System.out.println("In this lecture we will understand what is divide and conquer technique and how it works and why we need it");
-        
+        // it has maily three steps
+        // 1. divide the problem into smaller subproblems of similar type subproblem are also independent
+        // 2. conquer the subproblems by solving them recursively
+        // 3. combine the solutions of the subproblems to get the solution of the original problem
+
+        // how we decide that problem is large or not
+        // sometime by size of the input : like for 5 number we apply linear search but 5M number we apply binary search
+
+        // now if subproblems are overlap we will use dp
+
+
+
     }
 }
---
+++ DSA/src/AlgorithmDesignAndTechniques/DivideAndConquer/Lec2_BinarySearch_Using_Recursion_DAC.java
@@ -0,0 +1,25 @@
+package AlgorithmDesignAndTechniques.DivideAndConquer;
+
+public class Lec2_BinarySearch_Using_Recursion_DAC {
+    public static void main(String[] args) {
+        System.out.println("In this lecture we will understand how to use binary search using recursion in divide and conquer technique");
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 8;
+        int ans = binarySearch(arr, target, 0, arr.length - 1);
+        System.out.println(ans);
+        // now basically we are using divide and conquer technique
+        // we divide and find solution
+        
+
+    }
+
+    private static int binarySearch(int[] arr, int target, int low, int high) {
+        if(low > high ) return -1;
+        int mid = low + (high-low)/2;
+        if(arr[mid] == target) return mid;
+        else if(arr[mid] > target) binarySearch(arr,target,low,mid-1);
+        else binarySearch(arr,target,mid+1,high);
+        return -1;
+    }
+
+}
