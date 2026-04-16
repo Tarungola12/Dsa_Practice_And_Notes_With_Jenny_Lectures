7f3e25efe929ee8308a4bc1344b1dd3c694f85b7
Tarun Gola <tarungola205@gmail.com>
2026-04-16 11:27:03 +0530

    Starting Of Algorithm design and technique section Jenny Mam

---
+++ DSA/src/AlgorithmDesignAndTechniques/DivideAndConquer/Lec1_DivideAndConquerTechnique.java
@@ -0,0 +1,8 @@
+package AlgorithmDesignAndTechniques.DivideAndConquer;
+
+public class Lec1_DivideAndConquerTechnique {
+    public static void main(String[] args) {
+        System.out.println("In this lecture we will understand what is divide and conquer technique and how it works and why we need it");
+        
+    }
+}
---
+++ DSA/src/AlgorithmDesignAndTechniques/Lec1_Introduction_To_Algorithm_Design_And_Techniques.java
@@ -0,0 +1,20 @@
+package AlgorithmDesignAndTechniques;
+
+public class Lec1_Introduction_To_Algorithm_Design_And_Techniques {
+    public static void main(String[] args) {
+        System.out.println("In this lecture we will understand the need of algorithm design and techniques");
+        System.out.println("we will also understand the different types of algorithm design and techniques");
+        // what is algorithm : like steps of making tea is knows as algorithm
+        // so when we solve computational problems so there are step to solve that problem and that step is known as algorithm
+
+        // it will reduce time complexity and space complexity drastically
+        // there are different types of algorithm design and techniques
+        // 1. Brute Force
+        // 2. Divide and Conquer
+        // 3. Greedy Algorithm
+        // 4. Dynamic Programming
+        // 5. Backtracking
+        // 6. Branch and Bound
+
+    }
+}
